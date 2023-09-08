26fafe65f2044b1b3636fcaa1ffce27c5fff25a4
ArmadOon <pluhar.martin@outlook.com>
2023-09-08 18:55:17 +0200

    added method for get students by id

--- src/main/java/com/martinpluhy/demo/entity/Student.java
+++ src/main/java/com/martinpluhy/demo/entity/Student.java
@@ -1,16 +1,25 @@
 package com.martinpluhy.demo.entity;
 
 public class Student {
-
+    int studentId;
     String firstName;
     String lastName;
 
     public Student() {
     }
 
-    public Student(String firstName, String lastName) {
+    public Student(int studentId, String firstName, String lastName) {
+        this.studentId = studentId;
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
     }
 
     public String getFirstName() {
--- src/main/java/com/martinpluhy/demo/rest/StudentRestController.java
+++ src/main/java/com/martinpluhy/demo/rest/StudentRestController.java
@@ -2,6 +2,7 @@ package com.martinpluhy.demo.rest;
 
 import com.martinpluhy.demo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,17 @@ public class StudentRestController  {
     @GetMapping("/students")
     public List<Student> getStudents(){
         List<Student> theStudents = new ArrayList<>();
-        theStudents.add(new Student("Martin", "Pluhar"));
-        theStudents.add(new Student("Kristyna", "Chrastilova"));
-        theStudents.add(new Student("Jiri", "Babica"));
+        theStudents.add(new Student(1,"Martin", "Pluhar"));
+        theStudents.add(new Student(2,"Kristyna", "Chrastilova"));
+        theStudents.add(new Student(3,"Jiri", "Babica"));
         return  theStudents;
+    }
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable int studentId){
+        List<Student> theStudents = new ArrayList<>();
+        theStudents.add(new Student(1,"Martin", "Pluhar"));
+        theStudents.add(new Student(2,"Kristyna", "Chrastilova"));
+        theStudents.add(new Student(3,"Jiri", "Babica"));
+        return theStudents.get(studentId);
     }
 }
