f15f9fa7e05b86b88991c300a6465a83fc006746
ArmadOon <pluhar.martin@outlook.com>
2023-09-09 17:21:14 +0200

    Exception handler for generic error

--- src/main/java/com/martinpluhy/demo/rest/StudentRestController.java
+++ src/main/java/com/martinpluhy/demo/rest/StudentRestController.java
@@ -55,4 +55,14 @@ public class StudentRestController {
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
 
+    // ADd another exception handler ... to catch any exceptuon
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc ){
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
 }
