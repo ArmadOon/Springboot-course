9eff1f323adcb38051101dbac922287995f48943
ArmadOon <pluhar.martin@outlook.com>
2023-09-10 20:27:14 +0200

    update

--- src/main/java/com/martinpluhy/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/martinpluhy/demo/service/EmployeeServiceImpl.java
@@ -1,42 +1,51 @@
 package com.martinpluhy.demo.service;
 
+import com.martinpluhy.demo.dao.EmployeeRepository;
 import com.martinpluhy.demo.entity.Employee;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
-        this.employeeDAO = theEmployeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        this.employeeRepository = theEmployeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
-    @Transactional
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee = null;
+        if(result.isPresent()){
+            theEmployee = result.get();
+        }else {
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+        return theEmployee;
+
     }
 
     @Override
-    @Transactional
     public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
     @Override
-    @Transactional
     public void deleteById(int theId) {
-    employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
 }
