bae3a68560bfd0cd531ff85d7c510619b614e0e0
ArmadOon <pluhar.martin@outlook.com>
2023-10-15 21:15:26 +0200

    update

--- src/main/java/com/martinpluhy/demo/dao/EmployeeRepository.java
+++ src/main/java/com/martinpluhy/demo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.martinpluhy.demo.dao;
 
 import com.martinpluhy.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
