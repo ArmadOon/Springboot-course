6ea4e32366005696bd11a5273647387b7f191458
ArmadOon <pluhar.martin@outlook.com>
2023-11-12 18:36:53 +0100

    security config created)

--- src/main/java/com/martinpluhy/demo/security/DemoSecurityConfig.java
+++ src/main/java/com/martinpluhy/demo/security/DemoSecurityConfig.java
@@ -1,7 +1,19 @@
 package com.martinpluhy.demo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class DemoSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+    }
 }
