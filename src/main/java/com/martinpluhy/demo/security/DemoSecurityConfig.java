aff362c7baa4422e0ed44208af4080c5d16043fa
ArmadOon <pluhar.martin@outlook.com>
2023-11-16 07:46:13 +0100

    security filterchain, crud methods based on roles

--- src/main/java/com/martinpluhy/demo/security/DemoSecurityConfig.java
+++ src/main/java/com/martinpluhy/demo/security/DemoSecurityConfig.java
@@ -2,9 +2,12 @@ package com.martinpluhy.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -28,5 +31,17 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE", "MANAGER")
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurator ->
+                configurator
+                        .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/employees").hasRole("ADMIN")
+        )
     }
 }
